5d26405327723e87fb3a82d605433ebbee127efc
zhangzhenpeng <zhenpeng0506@163.com>
2018-04-13 15:51:35 +0800

    修改cofactorization_ML20M.py

--- src/Cofactorization_ML20M.py
+++ src/Cofactorization_ML20M.py
@@ -6,32 +6,32 @@ if __name__ == '__main__':
 
     # In[ ]:
 
-
     import itertools
     import glob
     import os
     import sys
+
     os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
     import numpy as np
     import matplotlib
+
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
 
     import pandas as pd
     from scipy import sparse
     import seaborn as sns
+
     sns.set(context="paper", font_scale=1.5, rc={"lines.linewidth": 2}, font='DejaVu Serif')
     import sys
-    sys.path.append('E:\workspace\source_code\cofactor\src')
 
+    sys.path.append('E:\workspace\source_code\cofactor\src')
 
     # In[2]:
 
-
     import cofacto
     import rec_eval
-
 
     # ### Construct the positive pairwise mutual information (PPMI) matrix
 
@@ -39,12 +39,9 @@ if __name__ == '__main__':
 
     # In[3]:
 
-
     DATA_DIR = 'E:\datasets\ml-20m\pro'
 
-
     # In[4]:
-
 
     unique_uid = list()
     with open(os.path.join(DATA_DIR, 'unique_uid.txt'), 'r') as f:
@@ -56,9 +53,7 @@ if __name__ == '__main__':
         for line in f:
             unique_sid.append(line.strip())
 
-
     # In[5]:
-
 
     n_items = len(unique_sid)
     n_users = len(unique_uid)
@@ -68,50 +63,38 @@ if __name__ == '__main__':
 
     # In[6]:
 
-
     def load_data(csv_file, shape=(n_users, n_items)):
         tp = pd.read_csv(csv_file)
         timestamps, rows, cols = np.array(tp['timestamp']), np.array(tp['uid']), np.array(tp['sid'])
-        seq = np.concatenate((rows[:, None], cols[:, None], np.ones((rows.size, 1), dtype='int'), timestamps[:, None]), axis=1)
+        seq = np.concatenate((rows[:, None], cols[:, None], np.ones((rows.size, 1), dtype='int'), timestamps[:, None]),
+                             axis=1)
         data = sparse.csr_matrix((np.ones_like(rows), (rows, cols)), dtype=np.int16, shape=shape)
         return data, seq
 
 
     # In[7]:
 
-
     train_data, train_raw = load_data(os.path.join(DATA_DIR, 'train.csv'))
-
 
     # In[8]:
 
-
     watches_per_movie = np.asarray(train_data.astype('int64').sum(axis=0)).ravel()
-
 
     # In[9]:
 
-
     print("The mean (median) watches per movie is %d (%d)" % (watches_per_movie.mean(), np.median(watches_per_movie)))
-
 
     # In[10]:
 
-
     user_activity = np.asarray(train_data.sum(axis=1)).ravel()
-
 
     # In[11]:
 
-
     print("The mean (median) movies each user wathced is %d (%d)" % (user_activity.mean(), np.median(user_activity)))
-
 
     # In[12]:
 
-
     vad_data, vad_raw = load_data(os.path.join(DATA_DIR, 'validation.csv'))
-
 
     # In[13]:
 
@@ -120,22 +103,23 @@ if __name__ == '__main__':
     plt.ylabel('Number of items that this user clicked on')
     plt.xlabel('User rank by number of consumed items')
     pass
-    
-    
+
+
     # In[14]:
-    
-    
+
+
     plt.semilogx(1 + np.arange(n_items), -np.sort(-watches_per_movie), 'o')
     plt.ylabel('Number of users who watched this movie')
     plt.xlabel('Movie rank by number of watches')
     plt.show()
     pass
-    
+
     '''
+
+
     # ### Generate co-occurrence matrix based on the user's entire watching history
 
     # In[17]:
-
 
     def _coord_batch(lo, hi, train_data):
         rows = []
@@ -151,7 +135,6 @@ if __name__ == '__main__':
 
     # In[18]:
 
-
     from joblib import Parallel, delayed
 
     batch_size = 5000
@@ -159,40 +142,32 @@ if __name__ == '__main__':
     start_idx = range(0, n_users, batch_size)
     end_idx = start_idx[1:] + [n_users]
 
-    #Parallel(n_jobs=8)(delayed(_coord_batch)(lo, hi, train_data) for lo, hi in zip(start_idx, end_idx))
-
+    # Parallel(n_jobs=8)(delayed(_coord_batch)(lo, hi, train_data) for lo, hi in zip(start_idx, end_idx))
 
     '''
-    
     for lo, hi in zip(start_idx, end_idx):
             _coord_batch(lo, hi, train_data)
-    
+
     pass
-    
     '''
+
     # In[ ]:
-
-
-
-
 
     # In[19]:
 
-    '''
     X = sparse.csr_matrix((n_items, n_items), dtype='float32')
-    
+
     for lo, hi in zip(start_idx, end_idx):
         coords = np.load(os.path.join(DATA_DIR, 'coo_%d_%d.npy' % (lo, hi)))
-        
+
         rows = coords[:, 0]
         cols = coords[:, 1]
-        
+
         tmp = sparse.coo_matrix((np.ones_like(rows), (rows, cols)), shape=(n_items, n_items), dtype='float32').tocsr()
         X = X + tmp
-        
+
         print("User %d to %d finished" % (lo, hi))
         sys.stdout.flush()
-    '''
 
     # Note: Don't forget to delete all the temporary coo_LO_HI.npy files
 
@@ -214,33 +189,25 @@ if __name__ == '__main__':
 
     # In[15]:
 
-
     # or co-occurrence matrix from the entire user history
     dir_predix = DATA_DIR
 
-
     # In[16]:
-
 
     data = np.load(os.path.join(dir_predix, 'coordinate_co_binary_data.npy'))
     indices = np.load(os.path.join(dir_predix, 'coordinate_co_binary_indices.npy'))
     indptr = np.load(os.path.join(dir_predix, 'coordinate_co_binary_indptr.npy'))
 
-
     # In[17]:
-
 
     X = sparse.csr_matrix((data, indices, indptr), shape=(n_items, n_items))
 
-
     # In[18]:
-
 
     float(X.nnz) / np.prod(X.shape)
 
 
     # In[19]:
-
 
     def get_row(Y, i):
         lo, hi = Y.indptr[i], Y.indptr[i + 1]
@@ -249,20 +216,15 @@ if __name__ == '__main__':
 
     # In[20]:
 
-
     count = np.asarray(X.sum(axis=1)).ravel()
-
 
     # In[21]:
 
-
     n_pairs = X.data.sum()
-
 
     # ### Construct the SPPMI matrix
 
     # In[22]:
-
 
     M = X.copy()
 
@@ -270,24 +232,18 @@ if __name__ == '__main__':
         lo, hi, d, idx = get_row(M, i)
         M.data[lo:hi] = np.log(d * n_pairs / (count[i] * count[idx]))
 
-
     # In[23]:
-
 
     M.data[M.data < 0] = 0
     M.eliminate_zeros()
 
-
     # In[24]:
 
-
     print float(M.nnz) / np.prod(M.shape)
-
 
     # Now $M$ is the PPMI matrix. Depending on the number of negative examples $k$, we can obtain the shifted PPMI matrix as $\max(M_{wc} - \log k, 0)$
 
     # In[25]:
-
 
     # number of negative samples
     k_ns = 1
@@ -303,30 +259,25 @@ if __name__ == '__main__':
     M_ns.data[M_ns.data < 0] = 0
     M_ns.eliminate_zeros()
 
-
     # In[26]:
     '''
-    
+
     plt.hist(M_ns.data, bins=50)
     plt.yscale('log')
     pass
-    
+
     '''
     # In[27]:
 
-
     float(M_ns.nnz) / np.prod(M_ns.shape)
-
 
     # ### Train the model
 
-
     # In[28]:
-
 
     scale = 0.03
 
-    n_components = 100
+    n_components = 10
     max_iter = 20
     n_jobs = 8
     lam_theta = lam_beta = 1e-5 * scale
@@ -336,7 +287,6 @@ if __name__ == '__main__':
 
     save_dir = os.path.join(DATA_DIR, 'ML20M_ns%d_scale%1.2E' % (k_ns, scale))
 
-
     # In[29]:
 
     reload(cofacto)
@@ -344,46 +294,41 @@ if __name__ == '__main__':
                             random_state=98765, save_params=True, save_dir=save_dir, early_stopping=True, verbose=True,
                             lam_theta=lam_theta, lam_beta=lam_beta, lam_gamma=lam_gamma, c0=c0, c1=c1)
 
-
     # In[30]
 
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
     coder.fit(train_data, M_ns, vad_data=vad_data, batch_users=5000, k=100)
-
 
     # In[31]:
 
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
     test_data, _ = load_data(os.path.join(DATA_DIR, 'test.csv'))
     test_data.data = np.ones_like(test_data.data)
 
-
     # In[32]:
 
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
     n_params = len(glob.glob(os.path.join(save_dir, '*.npz')))
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
     params = np.load(os.path.join(save_dir, 'CoFacto_K%d_iter%d.npz' % (n_components, n_params - 1)))
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
     U, V = params['U'], params['V']
 
     # In[33]:
 
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
-    #print 'Test Recall@20: %.4f' % rec_eval.recall_at_k(train_data, test_data, U, V, k=20, vad_data=vad_data)
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
-    #print 'Test Recall@50: %.4f' % rec_eval.recall_at_k(train_data, test_data, U, V, k=50, vad_data=vad_data)
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*+
-    #print 'Test NDCG@100: %.4f' % rec_eval.normalized_dcg_at_k(train_data, test_data, U, V, k=100, vad_data=vad_data)
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # print 'Test Recall@20: %.4f' % rec_eval.recall_at_k(train_data, test_data, U, V, k=20, vad_data=vad_data)
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # print 'Test Recall@50: %.4f' % rec_eval.recall_at_k(train_data, test_data, U, V, k=50, vad_data=vad_data)
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*+
+    # print 'Test NDCG@100: %.4f' % rec_eval.normalized_dcg_at_k(train_data, test_data, U, V, k=100, vad_data=vad_data)
 
-    #import ipdb; ipdb.set_trace()  # <--- *BAMF!*
-    #print 'Test MAP@100: %.4f' % rec_eval.map_at_k(train_data, test_data, U, V, k=100, vad_data=vad_data)
-
+    # import ipdb; ipdb.set_trace()  # <--- *BAMF!*
+    # print 'Test MAP@100: %.4f' % rec_eval.map_at_k(train_data, test_data, U, V, k=100, vad_data=vad_data)
 
     # In[34]:
 
-
-    #np.savez('CoFactor_K100_ML20M.npz', U=U, V=V)
+    # np.savez('CoFactor_K100_ML20M.npz', U=U, V=V)
 
 
 
